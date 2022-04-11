c6a1e5101bdd53578a7ebe92d6bf9c37468927a4
Alexk <morfem95@gmail.com>
2022-04-11 17:37:51 +0300

    edited

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-function factorial(n) {
-    return n ? n * factorial(n-1) : 1;
+function fact(n){
+    if(n > 0){
+        return n * fact(n-1);
+    }else if(n < 0){
+        console.log('Enter a positive number');
+    }else{
+        return 1;
+    }  
 }
-console.log(factorial(3));
+console.log(fact(1));
